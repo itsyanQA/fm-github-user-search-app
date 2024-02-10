8f982d07a0f8ad69ee731064d7213d20a5df4900
stron <yaneliyahu83@gmail.com>
2024-02-10 18:39:24 +0200

    chore: initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import "./index.scss";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Layout } from "./components/layout/Layout.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>
+);
